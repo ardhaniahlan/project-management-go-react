1fe623c89ac243bd477db12b8ba617cc654307b2
ardhaniahlan <ardhan.official16@gmail.com>
2026-06-19 08:16:35 +0700

    BE: jwt

---
+++ server/utils/jwt.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"project-management-be/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func GenerateTokenJWT(userID uint64, role string, email string, publicID uuid.UUID) (string, error) {
+	secret := config.AppConfig.JWTSecret
+	exp, err := time.ParseDuration(config.AppConfig.JWTExpired)
+	if err != nil {
+		return "", err
+	}
+	expired := time.Now().Add(exp)
+	
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role": role,
+		"email": email,
+		"public_id": publicID,
+		"exp": expired.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
